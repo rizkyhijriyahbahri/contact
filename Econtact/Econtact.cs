054e4e20edf4b68bb1cec5c3e0aa4264433d7e2c
rizky <rizky@RECROW>
2023-05-18 01:12:02 +0700

    Add project files.

---
+++ Econtact/Econtact.cs
@@ -0,0 +1,157 @@
+﻿using Econtact.econtactClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Econtact
+{
+    public partial class Econtact : Form
+    {
+        public Econtact()
+        {
+            InitializeComponent();
+        }
+        contactClass c = new contactClass();
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            
+            c.FirstName = txtboxFirstName.Text;
+            c.LastName = txtboxLastName.Text;
+            c.ContactNo = txtBoxContactNumber.Text;
+            c.Address = txtBoxAddress.Text;
+            c.Gender = cmbGender.Text;
+
+            
+            bool success = c.Insert(c);
+            if(success==true)
+            {
+                
+                MessageBox.Show("New Contact Successfully Inserted");
+                //Call the Clear Method Here
+                Clear();
+            }
+            else
+            {
+                
+                MessageBox.Show("Failed to add New Contact. Try Again.");
+            }
+            
+            DataTable dt = c.Select();
+            dgvContactList.DataSource = dt;
+        }
+
+        private void Econtact_Load(object sender, EventArgs e)
+        {
+            
+            DataTable dt = c.Select();
+            dgvContactList.DataSource = dt;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        
+        public void Clear()
+        {
+            txtboxFirstName.Text = "";
+            txtboxLastName.Text = "";
+            txtBoxContactNumber.Text = "";
+            txtBoxAddress.Text = "";
+            cmbGender.Text = "";
+            txtboxContactID.Text = "";
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            
+            c.ContactID = int.Parse(txtboxContactID.Text);
+            c.FirstName = txtboxFirstName.Text;
+            c.LastName = txtboxLastName.Text;
+            c.ContactNo = txtBoxContactNumber.Text;
+            c.Address = txtBoxAddress.Text;
+            c.Gender = cmbGender.Text;
+            
+            bool success = c.Update(c);
+            if(success==true)
+            {
+               
+                MessageBox.Show("Contact has been successfully Updated.");
+                
+                DataTable dt = c.Select();
+                dgvContactList.DataSource = dt;
+              
+                Clear();
+            }
+            else
+            {
+                
+                MessageBox.Show("Failed to Update Contact.Try Again.");
+            }
+        }
+
+        private void dgvContactList_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            txtboxContactID.Text = dgvContactList.Rows[rowIndex].Cells[0].Value.ToString();
+            txtboxFirstName.Text = dgvContactList.Rows[rowIndex].Cells[1].Value.ToString();
+            txtboxLastName.Text = dgvContactList.Rows[rowIndex].Cells[2].Value.ToString();
+            txtBoxContactNumber.Text = dgvContactList.Rows[rowIndex].Cells[3].Value.ToString();
+            txtBoxAddress.Text = dgvContactList.Rows[rowIndex].Cells[4].Value.ToString();
+            cmbGender.Text = dgvContactList.Rows[rowIndex].Cells[5].Value.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+           
+            Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+          
+            c.ContactID = Convert.ToInt32(txtboxContactID.Text);
+            bool success = c.Delete(c);
+            if(success==true)
+            {
+               
+                MessageBox.Show("Contact successfully deleted.");
+             
+                DataTable dt = c.Select();
+                dgvContactList.DataSource = dt;
+               
+                Clear();
+            }
+            else
+            {
+              
+                MessageBox.Show("Failed to Delete Dontact. Try Again.");
+            }
+        }
+        static string myconnstr = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+        private void txtboxSearch_TextChanged(object sender, EventArgs e)
+        {
+            
+            string keyword = txtboxSearch.Text;
+            
+            SqlConnection conn = new SqlConnection(myconnstr);
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM tbl_contact WHERE FirstName LIKE '%"+keyword+"%' OR LastName LIKE '%"+keyword+"%' OR Address LIKE '%"+keyword+"%'",conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dgvContactList.DataSource = dt;
+        }
+    }
+}
